0b2d1c37bc2e7f1dd862bd24224472ea3b35234d
Chetan Nada <call2chetannada@gmail.com>
2022-12-25 10:51:03 +0530

    Coding file added

---
+++ Session 1/Coding/App.js
@@ -0,0 +1,36 @@
+// Manipulate the HTML DOM using Javscript
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Namaste Everyone";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+// Manipulate the HTML DOM using React
+
+// Create nested React Elements
+const heading = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "heading"
+);
+const heading1 = React.createElement(
+  "h1",
+  {
+    id: "title",
+  },
+  "heading1"
+);
+
+const container = React.createElement(
+  "div",
+  {
+    id: "container",
+  },
+  [heading, heading1]
+);
+
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(container);
