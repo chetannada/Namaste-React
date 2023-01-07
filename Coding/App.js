c593fc5b3571783a161d28f4e7408b7ab72a9914
Chetan Nada <call2chetannada@gmail.com>
2023-01-08 01:31:26 +0530

    Chapter 03 - Laying the Foundation session code updated

--- Coding/App.js
+++ Coding/App.js
@@ -25,10 +25,10 @@
  * Transitive Dependencies
  */
 
-
 // imported react and reactdom from nodemodule folder
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import {React, createElement as ce} from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Create Header element like navbar using createElement
 /*
@@ -42,37 +42,65 @@ import ReactDOM from 'react-dom/client';
  * 
  * 
  */
-const heading = React.createElement(
-  "div",
-  {
-    className: "header",
-    key: "header",
-  },
-  [React.createElement(
-    "h1",
-    {
-      key: "Title",
-    },
-  "Title"), React.createElement(
-    "ul",
-    {
-      key: "ul",
-    },
-  [React.createElement(
-    "li",
-    {
-      key: "Home",
-    },
-  "Home"),React.createElement(
-    "li",
-    {
-      key: "About Us",
-    },
-  "About Us")])]
-)
+// const heading = ce(
+//   "div",
+//   {
+//     className: "header",
+//     key: "header",
+//   },
+//   [ce(
+//     "h1",
+//     {
+//       key: "Title",
+//     },
+//   "Title"), ce(
+//     "ul",
+//     {
+//       key: "ul",
+//     },
+//   [ce(
+//     "li",
+//     {
+//       key: "Home",
+//     },
+//   "Home"),ce(
+//     "li",
+//     {
+//       key: "About Us",
+//     },
+//   "About Us")])]
+// )
 
-  // create root using createRoot
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  // passing react element inside root
-  root.render(heading);
-  
+// create header element using JSX
+// JSX => React.createElement => Object => HTML (DOM) (babel does all the conversion)
+const heading = (
+  <h1 id="h1" key="h1">
+    This is JSX
+  </h1>
+);
+
+// React Component 
+// Functional component - new way of writing component 
+// Class component - old way of writing component
+
+// Title component is functional component
+const Title = () => {
+  return (
+    <h1 id="title" key="title">Namste React</h1>
+  )
+}
+// Header component is functional component
+const HeaderComponent = function (){
+  return (
+    <div>
+      <Title/>
+      {console.log(10)}
+    <h1>Namaste React Functional component</h1>
+    <h2>This is h2 tag</h2>
+    </div>
+  )
+}
+// create root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// passing react element inside root
+root.render(<HeaderComponent/>);
