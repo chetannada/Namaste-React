42a709c0651ede0218a8d0d98109972401819276
Chetan Nada <call2chetannada@gmail.com>
2023-01-07 11:54:38 +0530

    Create Header element like navbar using createElement

--- Coding/App.js
+++ Coding/App.js
@@ -1,5 +1,8 @@
+// ## Namaste React Course by Akshay Saini
+// # Chapter 03 - Laying the Foundation
+
 /*
- *
+ * This are the feature of Parcel
  * Created A Server
  * HMR - Hot Module Replacement
  * File Watcher algorithm - C++
@@ -26,31 +29,50 @@
 // imported react and reactdom from nodemodule folder
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Create Header element like navbar using createElement
+/*
+ * 
+<div class="header">
+<h1>Title</h1>
+<ul>
+<li>Home</li>
+<li>About Us</li>
+</ul>
+ * 
+ * 
+ */
 const heading = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
-    "heading1"
-  );
-  
-  const container = React.createElement(
+  "div",
+  {
+    className: "header",
+    key: "header",
+  },
+  [React.createElement(
     "div",
     {
-      id: "container",
+      key: "Title",
     },
-    [heading, heading1]
-  );
-  
+  "Title"), React.createElement(
+    "ul",
+    {
+      key: "ul",
+    },
+  [React.createElement(
+    "li",
+    {
+      key: "Home",
+    },
+  "Home"),React.createElement(
+    "li",
+    {
+      key: "About Us",
+    },
+  "About Us")])]
+)
+
   // create root using createRoot
   const root = ReactDOM.createRoot(document.getElementById("root"));
   // passing react element inside root
-  root.render(container);
+  root.render(heading);
   
