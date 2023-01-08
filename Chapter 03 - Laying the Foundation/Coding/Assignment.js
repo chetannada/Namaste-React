b4dacf8dbcc7af5fc64ddbc8c557cb92c1519b12
Chetan Nada <call2chetannada@gmail.com>
2023-01-08 14:12:00 +0530

    Question Solution Composition of Component (Add a component inside another)

--- Chapter 03 - Laying the Foundation/Coding/Assignment.js
+++ Chapter 03 - Laying the Foundation/Coding/Assignment.js
@@ -57,11 +57,27 @@ import ReactDOM from "react-dom/client";
 // };
 
 // Q: Pass attribute into the tag in JSX
+// const Header = () => {
+//   return (
+//     <div className="Title" key="title">
+//       <h1 style={{color:"blue"}} key="h1">This is h1 tag</h1>
+//       <h2 style={{color:"palevioletred"}} key="h2">This is h2 tag</h2>
+//       <h3 style={{color:"green"}} key="h3">This is h3 tag</h3>
+//     </div>
+//   );
+// };
+
+// Composition of Component (Add a component inside another)
+const AnotherComponent = function(){
+    return <h2> This is Another Component</h2>
+}
+
 const Header = () => {
   return (
     <div className="Title" key="title">
       <h1 style={{color:"blue"}} key="h1">This is h1 tag</h1>
       <h2 style={{color:"palevioletred"}} key="h2">This is h2 tag</h2>
+      <AnotherComponent/>
       <h3 style={{color:"green"}} key="h3">This is h3 tag</h3>
     </div>
   );
