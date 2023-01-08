186cae42d8eec76044dd250f6261c09dac5dcbd7
Chetan Nada <call2chetannada@gmail.com>
2023-01-08 13:59:31 +0530

    Quesstion Solution Pass attribute into the tag in JSX

--- Chapter 03 - Laying the Foundation/Coding/Assignment.js
+++ Chapter 03 - Laying the Foundation/Coding/Assignment.js
@@ -46,15 +46,27 @@ import ReactDOM from "react-dom/client";
 // );
 
 // Q: Create a functional component of the same with JSX
+// const Header = () => {
+//   return (
+//     <div className="Title" key="title">
+//       <h1 key="h1">This is h1 tag</h1>
+//       <h2 key="h2">This is h2 tag</h2>
+//       <h3 key="h3">This is h3 tag</h3>
+//     </div>
+//   );
+// };
+
+// Q: Pass attribute into the tag in JSX
 const Header = () => {
   return (
     <div className="Title" key="title">
-      <h1 key="h1">This is h1 tag</h1>
-      <h2 key="h2">This is h2 tag</h2>
-      <h3 key="h3">This is h3 tag</h3>
+      <h1 style={{color:"blue"}} key="h1">This is h1 tag</h1>
+      <h2 style={{color:"palevioletred"}} key="h2">This is h2 tag</h2>
+      <h3 style={{color:"green"}} key="h3">This is h3 tag</h3>
     </div>
   );
 };
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header />); // we can also write like this for functional component root.render(Header())
