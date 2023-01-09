069d24c2a7dcad11e6de53fc6e68bf6aa9e82f0e
Chetan Nada <call2chetannada@gmail.com>
2023-01-09 13:40:46 +0530

    Coding File Updated Chapter 02 - Igniting our App

---
+++ Chapter 02 - Igniting our App/Coding/App.js
@@ -0,0 +1,58 @@
+// ## Namaste React Course by Akshay Saini
+// # Chapter 02 - Igniting our App
+
+/*
+ *** Parcel Feature ***
+ * Created A Server
+ * HMR - Hot Module Replacement
+ * File Watcher algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our Code
+ * Dev abd Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev
+ * port Number
+ * Consistent Hashing Algorithm
+ * Zero Config
+ * Tree shaking
+ *
+ *
+ * Transitive Dependencies
+ */
+
+
+// imported react and reactdom from nodemodule folder
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const heading = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading"
+  );
+  const heading1 = React.createElement(
+    "h1",
+    {
+      id: "title",
+    },
+    "heading1"
+  );
+  
+  const container = React.createElement(
+    "div",
+    {
+      id: "container",
+    },
+    [heading, heading1]
+  );
+  
+  // create root using createRoot
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  // passing react element inside root
+  root.render(container);
