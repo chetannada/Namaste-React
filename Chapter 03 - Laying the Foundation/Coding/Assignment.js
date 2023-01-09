3ca3716d47023b272ab20328ac22ba794c351b35
Chetan Nada <call2chetannada@gmail.com>
2023-01-09 12:37:06 +0530

    Question Solution Create a Header Component from scratch using Functional Component

--- Chapter 03 - Laying the Foundation/Coding/Assignment.js
+++ Chapter 03 - Laying the Foundation/Coding/Assignment.js
@@ -3,7 +3,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./Assignment.css";
+import logo from "./logo.png";
+import userIcon from "./user icon.png";
 // Q: Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class "title")
 // const header = React.createElement(
 //   "div",
@@ -85,33 +87,60 @@ import ReactDOM from "react-dom/client";
 
 // `{TitleComponent}` vs `{<TitleComponent/>}` vs `{<TitleComponent></TitleComponent>}` in JSX.
 
-const element = <h1>This is React Element</h1>; // This is React element or {TitleComponent}
+// const element = <h1>This is React Element</h1>; // This is React element or {TitleComponent}
 
-const TitleElement = () => {
-  return <h2 style={{ color: "red" }}>This Title Element</h2>;
-}; // This is Title Component
+// const TitleElement = () => {
+//   return <h2 style={{ color: "red" }}>This Title Element</h2>;
+// }; // This is Title Component
+
+// const Header = () => {
+//   return (
+//     <div className="Title" key="title">
+//       {/* This is {TitleComponent} */}
+//       {element}
+//       <h1 style={{ color: "blue" }} key="h1">
+//         This is h1 tag
+//       </h1>
+//       {/* This is {<TitleComponent/>} */}
+//       <TitleElement/>
+//       <h2 style={{ color: "palevioletred" }} key="h2">
+//         This is h2 tag
+//       </h2>
+//       {/* This is {<TitleComponent></TitleComponent>}*/}
+//       <TitleElement></TitleElement>
+//       <h3 style={{ color: "green" }} key="h3">
+//         This is h3 tag
+//       </h3>
+//     </div>
+//   );
+// };
+
+/*
+Q: Create a Header Component from scratch using Functional Component with JSX
+- Add a Logo on Left
+- Add a search bar in middle
+- Add User icon on right
+- Add CSS to make it look nice
+*/
 
 const Header = () => {
-  return (
-    <div className="Title" key="title">
-      {/* This is {TitleComponent} */}
-      {element}
-      <h1 style={{ color: "blue" }} key="h1">
-        This is h1 tag
-      </h1>
-      {/* This is {<TitleComponent/>} */}
-      <TitleElement/>
-      <h2 style={{ color: "palevioletred" }} key="h2">
-        This is h2 tag
-      </h2>
-      {/* This is {<TitleComponent></TitleComponent>}*/}
-      <TitleElement></TitleElement>
-      <h3 style={{ color: "green" }} key="h3">
-        This is h3 tag
-      </h3>
-    </div>
-  );
-};
+    return(
+        <>
+        <header className="header">
+            <div className="left">
+                <img src={logo} alt="Logo" />
+            </div>
+            <div className="center">
+                <input className="input" type="text" placeholder="Search anything you want..."/>
+                <button type="submit">Submit</button>
+            </div>
+            <div className="right">
+                <img src={userIcon} alt="User Icon"/>
+            </div>
+        </header>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header />); // we can also write like this for functional component root.render(Header())
