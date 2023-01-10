536e1fdcd26606cc2d0c4ac1f6bd73911026c631
Chetan Nada <call2chetannada@gmail.com>
2023-01-10 12:30:43 +0530

    Coding Assignment Chapter 03 Project file Updated

--- Chapter 03 - Laying the Foundation/Coding/Assignment.js
+++ Chapter 03 - Laying the Foundation/Coding/Assignment.js
@@ -124,23 +124,29 @@ Q: Create a Header Component from scratch using Functional Component with JSX
 */
 
 const Header = () => {
-    return(
-        <>
-        <header className="header">
-            <div className="left">
-                <img src={logo} alt="Logo" />
-            </div>
-            <div className="center">
-                <input className="input" type="text" placeholder="Search anything you want..."/>
-                <button type="submit">Submit</button>
-            </div>
-            <div className="right">
-                <img src={userIcon} alt="User Icon"/>
-            </div>
-        </header>
-        </>
-    )
-}
+  return (
+    <>
+      <header className="header">
+        <div className="left">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div className="center">
+          <input
+            className="input"
+            type="text"
+            placeholder="Search anything you want..."
+          />
+          <button type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+        <div className="right">
+          <img src={userIcon} alt="User Icon" />
+        </div>
+      </header>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Header />); // we can also write like this for functional component root.render(Header())
