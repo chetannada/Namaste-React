2ae7e50aeb10379da6adb1d8eb420c5863c40293
Chetan Nada <call2chetannada@gmail.com>
2023-01-11 23:44:41 +0530

    Created AppLayout for Food Fire App Chapter 04

---
+++ Chapter 04 - Talk is Cheap, show me the code/Coding/App.js
@@ -0,0 +1,67 @@
+// ## Namaste React Course by Akshay Saini
+// Chapter 04 - Talk is Cheap, show me the code
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FoodFireLogo from "../Images/Food Fire Logo.png";
+
+/* My Food App structure will look like this, 
+            1) Header
+                - Logo
+                - Nav Items(right side)
+                - Cart
+            2) Body
+                - Search bar
+                - Restaurants List
+                    - Restaurant card
+                        - Image
+                        - Name
+                        - Rating
+            3) Footer
+                - Links
+                - Copyrights
+       
+*/
+
+const Title = () => (
+  <a href="/">
+    <img className="logo" src={FoodFireLogo} alt="Food Fire Logo" />
+  </a>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return <h4>Body</h4>;
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
