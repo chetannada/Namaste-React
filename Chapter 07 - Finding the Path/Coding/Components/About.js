c61328cf11ac3843d9dfabf258757fb7fc6f5a85
Chetan Nada <call2chetannada@gmail.com>
2023-02-25 22:52:29 +0530

    typo fixed in About us page

--- Chapter 07 - Finding the Path/Coding/Components/About.js
+++ Chapter 07 - Finding the Path/Coding/Components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="about-container">
       <div className="about-left">
         <h1>
-          Welcome to <br /> The world of <br /> <span>Testy & Fresh Food</span>
+          Welcome to <br /> The world of <br /> <span>Tasty & Fresh Food</span>
         </h1>
         <h4>
           "Better you will feel if you eat a Food<span>Fire</span> healthy meal"
--- Chapter 09 - Optimizing our App/Coding/Components/About.js
+++ Chapter 09 - Optimizing our App/Coding/Components/About.js
@@ -35,7 +35,7 @@ const About = () => {
         <div className="about-left">
           <h1>
             Welcome to <br /> The world of <br />{" "}
-            <span>Testy & Fresh Food</span>
+            <span>Tasty & Fresh Food</span>
           </h1>
           <h4>
             "Better you will feel if you eat a Food<span>Fire</span> healthy
