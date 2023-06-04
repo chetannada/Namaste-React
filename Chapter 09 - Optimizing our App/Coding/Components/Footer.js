89549425ddf35ccaa28bcf6ba82f8fed399c3a7c
Chetan Nada <call2chetannada@gmail.com>
2023-06-04 19:22:57 +0530

    Added Login Logout functionality using LocalStorage

--- Chapter 09 - Optimizing our App/Coding/Components/Footer.js
+++ Chapter 09 - Optimizing our App/Coding/Components/Footer.js
@@ -1,16 +1,22 @@
 // Footer component for footer section
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <div className="footer">
       Created By
       <i className="fa-solid fa-heart"></i>
-      <a href="https://www.linkedin.com/in/chetannada/" target="_blank" title="Chetan Nada's Linkedin Profile">
+      <a
+        href="https://www.linkedin.com/in/chetannada/"
+        target="_blank"
+        title="Chetan Nada's Linkedin Profile"
+      >
         Chetan Nada
       </a>
-      <i className="fa-solid fa-copyright"></i>2023
-      <strong>
-        Food<span>Fire</span>
-      </strong>
+      <i className="fa-solid fa-copyright"></i>
+        {year}
+        <strong>
+          Food<span>Fire</span>
+        </strong>
     </div>
   );
 };
