8b602576273cab75166f8baf5dda39c16fbab4a9
Prathamesh Dhavale <56761941+prathmesh-cpu@users.noreply.github.com>
2023-12-31 20:44:37 +0530

    Update constants.js

--- Chapter 06 - Exploring the world/Coding/constants.js
+++ Chapter 06 - Exploring the world/Coding/constants.js
@@ -4,6 +4,6 @@ export const IMG_CDN_URL =
 
 // Swiggy API to get Restaurant data with corsproxy
 export const swiggy_api_URL =
-  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
 export const shimmer_card_unit = 20;
