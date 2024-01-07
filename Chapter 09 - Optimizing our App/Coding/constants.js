55ba5bbd89ff9445419fa58abc84040eef8deb71
Chetan Nada <call2chetannada@gmail.com>
2024-01-08 01:40:33 +0530

    API Issue Fixed by creating FoodFire Web Server

--- Chapter 09 - Optimizing our App/Coding/constants.js
+++ Chapter 09 - Optimizing our App/Coding/constants.js
@@ -8,11 +8,11 @@ export const ITEM_IMG_CDN_URL =
 
 // Swiggy API to get Restaurant data with thingproxy
 export const swiggy_api_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+  "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
 // Swiggy API to get Restaurant Menu data with thingproxy
 export const swiggy_menu_api_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
 // shimmer card unit
 export const shimmer_card_unit = 20;
