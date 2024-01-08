813eea42ac8236d029992ae528eced8d8d8daf7a
Chetan Nada <call2chetannada@gmail.com>
2024-01-08 23:03:10 +0530

    API Issue Fixed in Chapter 07

--- Chapter 07 - Finding the Path/Coding/constants.js
+++ Chapter 07 - Finding the Path/Coding/constants.js
@@ -6,13 +6,13 @@ export const IMG_CDN_URL =
 export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-// Swiggy API to get Restaurant data with thingproxy
+// Swiggy API to get Restaurant data with foodfire erver
 export const swiggy_api_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
+  "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
-// Swiggy API to get Restaurant Menu data with thingproxy
+// Swiggy API to get Restaurant Menu data with foodfire erver
 export const swiggy_menu_api_URL =
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
+  "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
 // shimmer card unit
 export const shimmer_card_unit = 20;
