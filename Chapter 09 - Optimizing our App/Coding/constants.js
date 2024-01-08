4a9bff5c964e36a98168e1812607ca4e5cd5a99f
Chetan Nada <call2chetannada@gmail.com>
2024-01-08 23:12:23 +0530

    Comment Text Changes

--- Chapter 09 - Optimizing our App/Coding/constants.js
+++ Chapter 09 - Optimizing our App/Coding/constants.js
@@ -6,11 +6,11 @@ export const IMG_CDN_URL =
 export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
-// Swiggy API to get Restaurant data with thingproxy
+// Swiggy API to get Restaurant data with foodfire server
 export const swiggy_api_URL =
   "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING";
 
-// Swiggy API to get Restaurant Menu data with thingproxy
+// Swiggy API to get Restaurant Menu data with foodfire server
 export const swiggy_menu_api_URL =
   "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
 
