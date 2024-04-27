76b349880b7eb0e934e4e996b02676b27a681935
Vivekanand Vernekar <bendragon21@gmail.com>
2024-04-27 13:57:32 +0530

    Fixed Image Loading Issue
    Fixed broken API links which caused the issue of images of restaurants, as well as menu items not loading.

--- Chapter 09 - Optimizing our App/Coding/constants.js
+++ Chapter 09 - Optimizing our App/Coding/constants.js
@@ -1,10 +1,10 @@
 // Image CDN URL for Restaurant card
 export const IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 // Image CDN URL for Restaurant Menu
 export const ITEM_IMG_CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
 
 // Swiggy API to get Restaurant data with foodfire server
 export const swiggy_api_URL =
